b8fc2f8fbed3b2616efa1ace8133ad648ab95554
书帆 龚 <2020302111121@whu.edu.cn>
2021-04-11 12:09:13 +0800

    update FindComposite function;

--- FindComposite.h
+++ FindComposite.h
@@ -1,8 +1,20 @@
 #pragma once
 #include <iostream>
+#include <regex>
+#include <string>
 
 using namespace std;
 
+//aeg: string that splited by ' ', e.g. A(x) + B(x) -> A(x)\n B(x)
 void FindComposite(string funcs) {
+    string str(funcs);
 
+    regex e(" \\+ ");
+
+    sregex_token_iterator pos(str.cbegin(), str.cend(), e, -1); // 定义regex_token_iterator
+    sregex_token_iterator end; //序列结束
+
+    for (; pos != end; pos++) {
+        cout << *pos << endl;
+    }
 }
